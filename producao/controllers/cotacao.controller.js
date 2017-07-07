fea35f1cc2c6117b2ef363523a2408e6fbf85311
Leo Brescia <leo@basic.com.br>
2017-07-07 13:56:39 -0300

    cotacao

--- producao/controllers/cotacao.controller.js
+++ producao/controllers/cotacao.controller.js
@@ -5,11 +5,13 @@
     .module('app')
     .controller('CotacaoController', CotacaoController);
 
-  CotacaoController.$inject = ['$rootScope', '$state'];
+  CotacaoController.$inject = ['$rootScope', '$state', 'rastreadorCarro', 'rastreadorMoto', '$http', 'api'];
 
-  function CotacaoController($rootScope, $state) {
+  function CotacaoController($rootScope, $state, rastreadorCarro, rastreadorMoto, $http, api) {
     var vm = this;
 
+    vm.carregando = true;
+    vm.hasRastreador = false;
     vm.preco = {
       basico: 'R$29,90',
       bronze: undefined,
@@ -23,7 +25,61 @@
     function Activate() {
       if (!$rootScope.usuario) {
         $state.go('placa');
+      } else {
+        GetPrecos();
       }
+    }
+
+    function GetPrecos() {
+      var filtro = 'Carro';
+      var valores = [];
+      var valorFipe = $rootScope.usuario.preco.replace('R$ ', ' ');
+      valorFipe = valorFipe.replace(',00', ' ');
+      valorFipe = parseInt(valorFipe);
+
+      console.log($rootScope.usuario);
+
+      if ($rootScope.usuario.veiculo === 'AUTOMÓVEL') {
+
+        if (valorFipe > rastreadorCarro) {
+          vm.hasRastreador = true;
+        }
+
+        if ($rootScope.usuario.especial) {
+          filtro = 'Especial';
+        }
+      } else {
+        filtro = 'Moto';
+        if (valorFipe > rastreadorMoto) {
+          vm.hasRastreador = true;
+        }
+      }
+
+      $http.get(api + 'preco?filter=veiculo,eq,' + filtro).then(function (resp) {
+        valores = php_crud_api_transform(resp.data).preco;
+        
+        angular.forEach(valores, function (value, key) {
+          if (valorFipe >= parseInt(value.min) && valorFipe <= parseInt(value.max)) {
+            switch (value.plano) {
+              case 'Bronze':
+                vm.preco.bronze = value.valor;
+                break;
+
+              case 'Prata':
+                vm.preco.prata = value.valor;
+                break;
+
+              case 'Ouro':
+                vm.preco.ouro = value.valor;
+                break;
+
+              default:
+                break;
+            }
+          }
+        });
+        vm.carregando = false;
+      });
     }
   }
 })();
