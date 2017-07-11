454e3b5735b25d6c1ea22a363c9530c30ce71096
Leo Brescia <leo@basic.com.br>
2017-07-11 11:15:36 -0300

    doc

--- producao/services/checkCondition.service.js
+++ producao/services/checkCondition.service.js
@@ -7,34 +7,64 @@
 
   CheckConditionService.$inject = ['anoCarro', 'anoMoto'];
 
+  /**
+   * @memberof app
+   * @ngdoc factory
+   * @name CheckConditionService
+   * @author Leo Brescia <leonardo@leobrescia.com.br>
+   * @desc Serviço para checar se o carro ou a moto eh valido
+   * 
+   * @param {constant} anoCarro  - ano do carro para validação
+   * @param {constant} anoMoto   - ano da moto para validação
+   * 
+   * TODO: Validar modelos de motos
+   * 
+   * @see Veja [Angular DOC]    {@link https://docs.angularjs.org/guide/providers#factory-recipe} Para mais informações
+   * @see Veja [John Papa DOC]  {@link https://github.com/johnpapa/angular-styleguide/tree/master/a1#factories} Para melhores praticas
+   */
   function CheckConditionService(anoCarro, anoMoto) {
-    var ano = '';
+    var ano        = '';
     var fabricante = '';
-    var modelo = '';
+    var modelo     = '';
     var rejeitados = [];
 
     var service = {
-      Activate: Activate,
-      CarHasValidModel: CarHasValidModel,
-      CarHasValidYear: CarHasValidYear,
+      Activate:          Activate,
+      CarHasValidModel:  CarHasValidModel,
+      CarHasValidYear:   CarHasValidYear,
       MotoHasValidModel: MotoHasValidModel,
-      MotoHasValidYear: MotoHasValidYear
+      MotoHasValidYear:  MotoHasValidYear
     };
 
     return service;
 
     ////////////////
 
+    /**
+     * @function Activate
+     * @desc Construtor do service. 
+     * @param {string} modelo - modelo do veiculo
+     * @param {string} fabricante - fabricante do veiculo
+     * @param {string} ano - ano do veiculo
+     * @param {json}   rej - lista de veiculos rejeitados
+     * @memberof CheckConditionService
+     */
     function Activate(modelo, fabricante, ano, rej) {
-      this.ano = ano;
+      this.ano        = ano;
       this.fabricante = fabricante;
-      this.modelo = modelo;
+      this.modelo     = modelo;
       this.rejeitados = rej;
     }
 
+    /**
+     * @function CarHasValidModel
+     * @desc Verifica se o modelo do carro eh valido
+     * @return {boolean} 
+     * @memberof CheckConditionService
+     */
     function CarHasValidModel() {
       var retorno = true;
-      console.log(this.fabricante);
+
       switch (this.fabricante) {
         case 'Audi':
           retorno = (parseInt(this.ano) > 2008);
@@ -77,7 +107,6 @@
           break;
         case 'VM - VolksWagen':
           retorno = (parseInt(this.ano) > 2000);
-          console.log((parseInt(this.ano) > 2000));
           break;
         case 'Volvo':
           retorno = (parseInt(this.ano) > 2008);
@@ -91,10 +120,10 @@
       if (retorno) {
         angular.forEach(rejeitados, function (value, key) {
           if (value.Fabricante == this.fabricante) {
-            var modeloTeste = this.modelo;
+            var modeloTeste     = this.modelo;
             var modeloRejeitado = value.Modelo;
 
-            modeloTeste = modeloTeste.toUpperCase();
+            modeloTeste     = modeloTeste.toUpperCase();
             modeloRejeitado = modeloRejeitado.toUpperCase();
 
             if (modeloTeste.includes(modeloRejeitado)) {
@@ -106,16 +135,34 @@
       return retorno;
     }
 
+    /**
+     * @function CarHasValidYear
+     * @desc Verifica se o ano do carro eh valido
+     * @return {boolean}
+     * @memberof CheckConditionService
+     */
     function CarHasValidYear() {
       var yearsApart = new Date(new Date() - new Date(this.ano + '-01-01')).getFullYear() - 1970;
 
       return (yearsApart < anoCarro);
     }
 
+    /**
+     * @function MotoHasValidModel
+     * @desc Verifica se o modelo da moto eh valido
+     * @return {boolean}
+     * @memberof CheckConditionService
+     */
     function MotoHasValidModel() {
       return true;
     }
 
+    /**
+     * @function MotoHasValidYear
+     * @desc Verifica se o ano da moto eh valido
+     * @return {boolean} 
+     * @memberof CheckConditionService
+     */
     function MotoHasValidYear() {
       return (parseInt(this.ano) > anoMoto);
     }
