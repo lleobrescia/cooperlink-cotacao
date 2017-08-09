123fe1d6eab454d9b3e375da9bf5c436b96efc13
Leo Brescia <leo@basic.com.br>
2017-08-09 15:06:44 -0300

    novo input mask

--- dev/app/app.module.js
+++ dev/app/app.module.js
@@ -8,17 +8,17 @@
    * @author Leo Brescia <leonardo@leobrescia.com.br>
    * @desc Modulo principal da cotação.<br>
    * 
-   * @param {module} ngMask     - Mascara para inputs {@link https://github.com/candreoliveira/ngMask}
-   * @param {module} ngAnimate  - Animação no angular {@link https://docs.angularjs.org/api/ngAnimate}
-   * @param {module} ui.router  - Rotas do Angular(troca de view) {@link https://github.com/angular-ui/ui-router}
-   * @param {module} ngMessages - Alertas nos inputs {@link https://docs.angularjs.org/api/ngMessages/directive/ngMessages}
-   * @param {module} toaster    - Popup de mensagem {@link https://github.com/jirikavi/AngularJS-Toaster}
+   * @param {module} ui.utils.masks     - Mascara para inputs {@link https://github.com/assisrafael/angular-input-masks}
+   * @param {module} ngAnimate          - Animação no angular {@link https://docs.angularjs.org/api/ngAnimate}
+   * @param {module} ui.router          - Rotas do Angular(troca de view) {@link https://github.com/angular-ui/ui-router}
+   * @param {module} ngMessages         - Alertas nos inputs {@link https://docs.angularjs.org/api/ngMessages/directive/ngMessages}
+   * @param {module} toaster            - Popup de mensagem {@link https://github.com/jirikavi/AngularJS-Toaster}
    * 
    * @see Veja [Angular DOC]    {@link https://docs.angularjs.org/guide/module} Para mais informações
    * @see Veja [John Papa DOC]  {@link https://github.com/johnpapa/angular-styleguide/tree/master/a1#startup-logic} Para melhores praticas
    */
   angular.module('app', [
-    'ngMask',
+    'ui.utils.masks',
     'ngAnimate',
     'ui.router',
     'ngMessages',
